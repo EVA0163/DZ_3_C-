b2a98308f156b34fa349a0da200744bf5bc98b2c
EVA0163 <frolov.sergey1917@gmail.com>
2022-10-07 20:20:02 +0500

    ДЗ сделанно

--- Zadacha19/Program1.cs
+++ Zadacha19/Program1.cs
@@ -1,11 +1,20 @@
-﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-// 456 -> 5
-// 782 -> 8
-// 918 -> 1
+﻿Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int len = number.Length;
 
-Console.Clear();
-Console.WriteLine("Задача 10 ");
-Console.Write("Введи трёхзначное число: ");
-int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
-string stringNumber = Convert.ToString(threeDigitNumber);
-Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
+if (len  == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - Палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - НЕ палиндром");
+    }
+}
+else
+{
+    Console.WriteLine($"ОШИБКА: {number} - не является пятизначным");
+}
+
